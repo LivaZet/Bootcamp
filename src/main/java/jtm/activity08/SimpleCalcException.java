a2b72691318f9ecd552fa307d2c794c836e86f56
Liva.Zet <liva.zankovska@gmail.com>
2020-02-10 21:18:41 +0200

    Activity 08 finished

---
+++ src/main/java/jtm/activity08/SimpleCalcException.java
@@ -0,0 +1,12 @@
+package jtm.activity08;
+
+public class SimpleCalcException extends java.lang.Exception {
+
+    public SimpleCalcException(String msg) {
+        super(msg);
+    }
+
+    public SimpleCalcException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
