c75abdcd390651d4d2a92bc7ed9a1cbf4a26ce41
Liva.Zet <liva.zankovska@gmail.com>
2020-02-15 10:42:06 +0200

    Activity 10

---
+++ src/main/java/jtm/activity10/FileCopy.java
@@ -0,0 +1,34 @@
+package jtm.activity10;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileCopy {
+
+    /*
+        TODO
+        Implement method that takes path to file as a parameter
+        And copies whole contents of the file to the same location
+        but with a new file name: <old_file_name>_backup e.g.
+        if file is data.txt copy should be named data.txt_backup
+     */
+    public void copyFile(String filePath) {
+        try {
+            List<String> linesInList = Files.readAllLines(Paths.get(filePath));
+            Files.write(Paths.get(filePath + "_backup"),linesInList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String path = "/home/student/Downloads/JTM_LZ/src/main/resources/data.json";
+        File file = new File(path);
+        System.out.println("Reading from: " + file.getAbsolutePath());
+        new FileCopy().copyFile(path);
+
+    }
+}
